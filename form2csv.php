fec60418e7f03f49a80d3c3d0330d2d5e26b2101
Mark D. Thompson <thomesoni@gmail.com>
2014-04-04 10:50:43 -0400

    Create form2csv.php

---
+++ form2csv.php
@@ -0,0 +1,34 @@
+<?php
+header('Content-type: application/x-javascript');
+
+
+/* validate email address using regular expression */
+function validateEmail($address){
+	$pattern = '/^(?!(?:(?:\\x22?\\x5C[\\x00-\\x7E]\\x22?)|(?:\\x22?[^\\x5C\\x22]\\x22?)){255,})(?!(?:(?:\\x22?\\x5C[\\x00-\\x7E]\\x22?)|(?:\\x22?[^\\x5C\\x22]\\x22?)){65,}@)(?:(?:[\\x21\\x23-\\x27\\x2A\\x2B\\x2D\\x2F-\\x39\\x3D\\x3F\\x5E-\\x7E]+)|(?:\\x22(?:[\\x01-\\x08\\x0B\\x0C\\x0E-\\x1F\\x21\\x23-\\x5B\\x5D-\\x7F]|(?:\\x5C[\\x00-\\x7F]))*\\x22))(?:\\.(?:(?:[\\x21\\x23-\\x27\\x2A\\x2B\\x2D\\x2F-\\x39\\x3D\\x3F\\x5E-\\x7E]+)|(?:\\x22(?:[\\x01-\\x08\\x0B\\x0C\\x0E-\\x1F\\x21\\x23-\\x5B\\x5D-\\x7F]|(?:\\x5C[\\x00-\\x7F]))*\\x22)))*@(?:(?:(?!.*[^.]{64,})(?:(?:(?:xn--)?[a-z0-9]+(?:-+[a-z0-9]+)*\\.){1,126}){1,}(?:(?:[a-z][a-z0-9]*)|(?:(?:xn--)[a-z0-9]+))(?:-+[a-z0-9]+)*)|(?:\\[(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){7})|(?:(?!(?:.*[a-f0-9][:\\]]){7,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,5})?)))|(?:(?:IPv6:(?:(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){5}:)|(?:(?!(?:.*[a-f0-9]:){5,})(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3})?::(?:[a-f0-9]{1,4}(?::[a-f0-9]{1,4}){0,3}:)?)))?(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))(?:\\.(?:(?:25[0-5])|(?:2[0-4][0-9])|(?:1[0-9]{2})|(?:[1-9]?[0-9]))){3}))\\]))$/iD';
+
+	if (preg_match($pattern, $address) === 1) {
+		return true;
+	}
+
+	return false;
+}
+
+// Check if the "from" input field is filled out
+if (isset($_GET["from"]) && validateEmail($_GET["from"])) {
+
+	$from = $_GET["from"]; // sender
+	$subject = $_GET["subject"]; // email subject
+	$message = $_GET["message"]; // email text message
+	$callback = $_GET["callback"]; // javascript callback function
+
+	// message lines should not exceed 70 characters (PHP rule), so wrap it
+	$message = wordwrap($message, 70);
+
+	$fp = fopen('data.csv', 'w');
+        fputcsv($fp, $from . "," . $subject . "," . $message);
+    	fclose($fp);
+
+} else {
+	echo $callback."([{status:'invalid email',message:'Invalid email address.'}])";
+}
+?>
